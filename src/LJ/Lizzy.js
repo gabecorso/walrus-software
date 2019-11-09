d61237e635a3d512b5b164c0e1cb3a0d2350e9a0
Gabe Corso <gcorso@massiveu.com>
2019-11-09 12:12:25 -0500

    .

--- src/LJ/Lizzy.js
+++ src/LJ/Lizzy.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Card, Jumbotron, Container, Button} from "react-bootstrap";
+import {Card, Jumbotron, Container, Button, Row, Col} from "react-bootstrap";
 import { FaBeer, FaHandPeace  } from 'react-icons/fa';
 import './styles/core/core.css'
 import './styles/lj.css'
@@ -16,32 +16,33 @@ class Lizzy extends React.Component {
         let birdTitle = 's';
 
         return (
-            <Container>
-                <div style={{display: 'none'}}>
-                <iframe className={'box-shadow'}  width="560" height="315" src="https://www.youtube.com/embed/Z0-VuyIz3Qg?autoplay=1" frameBorder="0"
-                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen></iframe>
-                </div>
+            <Container className={'.impact-text'}>
+                <Row>
+                    <Col xs={12} className={'p-2'}>
+                        <div style={{display: 'none'}}>
+                        <iframe className={'box-shadow'}  width="560" height="315" src="https://www.youtube.com/embed/Z0-VuyIz3Qg?autoplay=1" frameBorder="0"
+                                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                                allowFullScreen></iframe>
+                        </div>
 
-                <div style={{display: 'inline'}}>
-                    <div   className={'lizzy brutal-text'}>
-                        <h1>Hello Lizzy</h1>
-                    </div>
-                    <div style={{marginLeft: '50%', marginTop: '-7%'}}>
-                        <FaHandPeace size={50} style={{color: 'gold'}} className={'glitch spin-cycle'} />
-                    </div>
-                </div>
+                        <div style={{display: 'inline'}}>
+                            <div   className={'lizzy brutal-text'}>
+                                <h1>Hello Lizzy</h1>
+                            </div>
+                            <div style={{marginLeft: '50%', marginTop: '-7%'}}>
+                                <FaHandPeace size={50} style={{color: 'gold'}} className={'glitch spin-cycle'} />
+                            </div>
+                        </div>
 
-                <div className={'anim-bg-water'} style={{marginTop: 50, width: 500, height: 300, textAlign: 'center', padding: '5%', backgroundColor: 'tomato', borderRadius: 5}}>
-                    <h4><strong style={{color: 'tomato'}}>tryna buy some hip ass t-shirts or socks or something lmao? we have retro too...</strong></h4>
-                </div>
+                        <div className={'anim-bg-water cta-container'}>
+                            <h4><strong style={{color: 'tomato'}}>hotdeals could go here</strong></h4>
+                        </div>
 
-                <Button className={'button-out mt-5 ml-5'}>
-                    Purchase!
-                </Button>
-
-
-
+                        <Button className={'button-out mt-5 ml-5'}>
+                            Purchase!
+                        </Button>
+                    </Col>
+                </Row>
             </Container>
         )
     }
